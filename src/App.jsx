631467e5092d742aa8a33a5821c091acaa042836
KirillLaptop <sinty.kpov@gmail.com>
2023-11-30 13:35:29 +0300

    corrections

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import ShopItemFunc from './ShopItemFunc'
 
 const item = {
   brand: 'Tiger of Sweden',
@@ -7,23 +8,6 @@ const item = {
   descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
   price: 399,
   currency: '£'
-}
-
-function ShopItemFunc() {
-  return(
-    <div className="main-content">
-    <h2>{item.brand}</h2>
-    <h1>{item.title}</h1>
-    <h3>{item.description}</h3>
-    <div className="description">{item.descriptionFull}</div>
-    <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
-    <div className="divider"></div>
-    <div className="purchase-info">
-      <div className="price">{item.currency}{item.price}.00</div>
-      <button>Добавить в корзину</button>
-    </div>
-  </div>
-  )
 }
 
 function App() {
